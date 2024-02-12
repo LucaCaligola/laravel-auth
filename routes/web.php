b28c120e774e35021b2c488a5ece3d95a9d1fdd7
LucaCaligola <luca.caligola@gmail.com>
2024-02-12 16:44:41 +0100

    DB table project and ProjectSeeder created also changes to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->get('/home', [AdminDashboardController::class,'index '])->name('home');
+
